60400a8effd5d9a1036f5863bf52dbb2aa766b23
MalLunBar <malinelundgren1991@gmail.com>
2025-04-22 16:06:45 +0200

    starting with styled-components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,32 @@
-import "./Button.css"
- 
+import styled from "styled-components"
 
-const Button = ({ label, url }) => {
+
+const ButtonStyled = styled.a`
+ background-color: black; 
+ color: white; 
+ border-radius: 12px;
+ width: 303px; 
+ height: 48px;
+ font-size: 18px;
+ padding: 16px; 
+ display: flex;
+ gap: 16px; 
+ align-items: center;
+
+`
+
+const IconStyled = styled.img`
+  width: 31px;
+  height: 31px;
+`
+
+
+const Button = ({ icon, label, url }) => {
   return (
-    <>
-      <a href={url} target="_blank">
-        <button>{label}</button>
-      </a>
-    </>
+    <ButtonStyled href={url} target="_blank" rel="noopener noreferrer">
+      {icon && <IconStyled src={icon} alt="" />}
+      {label}
+    </ButtonStyled>
   )
 
 }
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,7 +13,7 @@ const ProjectCard = ({ image, tags, name, description, text, netlify, github }) 
       </div>
       <h2>{name}</h2>
       <p>{description}</p>
-      <Button label="Live Demo" url={netlify} />
+      <Button icon="assets/Icon-Web.svg" label="Live Demo" url={netlify} />
       <Button label="View Code" url={github} />
     </article>
   )
